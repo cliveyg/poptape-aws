5935f3f9efa91d365bb1c63d5eed481f42cc06a0
cliveyg <junkcat@gmail.com>
2025-05-27 19:05:05 -0300

    Adding API tests

--- app/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-from unittest.mock import patch, MagicMock
-
-# Patch all decorators and validators globally BEFORE importing anything else
-patch("app.decorators.require_access_level", lambda *a, **kw: (lambda f: f)).start()
-patch("app.main.views.require_access_level", lambda *a, **kw: (lambda f: f)).start()
-patch("app.assertions.assert_valid_schema", lambda data, schema: True).start()
-patch("app.extensions.boto3.client").start()
-patch("requests.post").start()
-patch("requests.get").start()

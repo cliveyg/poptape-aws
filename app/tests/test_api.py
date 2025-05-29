48df81f4ea75a777005a41d0a07f2cdaa5bf4de0
cliveyg <junkcat@gmail.com>
2025-05-29 14:01:46 -0300

    Add more tests

--- app/tests/test_api.py
+++ app/tests/test_api.py
@@ -126,6 +126,24 @@ class MyTest(FlaskTestCase):
 
     # -----------------------------------------------------------------------------
 
+    def test_boto_fail(self):
+
+        # valid payload with a random UUID
+        os.environ.pop('AWS_SECRET_ACCESS_KEY')
+        payload = { "public_id": str(uuid.uuid4()) }
+        headers = { 'Content-type': 'application/json', 'x-access-token': 'somefaketoken' }
+        response = self.client.post(
+            "/aws/user",
+            data=json.dumps(payload),
+            headers=headers,
+        )
+
+        self.assertTrue(response.status_code, 201)
+        self.assertTrue("User created on AWS" in response.get_data(as_text=True))
+        os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
+
+    # -----------------------------------------------------------------------------
+
     def test_create_user_fail_invalid_input(self):
 
         headers = { 'Content-type': 'application/json', 'x-access-token': 'somefaketoken' }
